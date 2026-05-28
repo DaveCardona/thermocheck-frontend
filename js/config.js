b1680372057d43a3667bc91731d697507493bd6c
Davewaves <davidcardona0923@gmail.com>
2026-05-27 22:01:09 -0500

    manejo inputs

--- js/config.js
+++ js/config.js
@@ -1,13 +1,14 @@
 //PRODUCCION
 
+
 const CONFIG = {
   API_URL: "https://themocheck-backend-production.up.railway.app"
 };
 
 
+/*
 // LOCAL
 
-/*
 const CONFIG = {
   API_URL: "http://localhost:3000"
 };
